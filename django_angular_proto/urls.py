08786155383b2b9b28b55da66f117f65d0e6713d
sundar_g <gnanasundar.g@siddhanintelligence.com>
2023-08-25 00:23:02 +0530

    'check'

--- django_angular_proto/urls.py
+++ django_angular_proto/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('health', views.health_check, name = "health_check"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
