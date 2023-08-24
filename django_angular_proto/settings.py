7edc93e8bcb41ac1f9c96b9990cb3a93957a06d1
sundar_g <gnanasundar.g@siddhanintelligence.com>
2023-08-24 17:26:02 +0530

    'changing_root_directory'

--- django_angular_proto/settings.py
+++ django_angular_proto/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'angular'),  # Path to your Angular build output
+    os.path.join(BASE_DIR, '..','angular'),  # Path to your Angular build output
 ]
 
 # Default primary key field type
