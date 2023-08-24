db453ebcaa4b0ee51cbb8e2ccd26976a7ca68457
sundar_g <gnanasundar.g@siddhanintelligence.com>
2023-08-24 22:57:53 +0530

    'fix'

--- django_angular_proto/urls.py
+++ django_angular_proto/urls.py
@@ -20,7 +20,7 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='static/index.html')),
     path('admin/', admin.site.urls),
     path('health', views.health_check, name = "health_check"),
 ]
