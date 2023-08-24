b94552f1f204f835264ef13f50f64730c2d787e6
sundar_g <gnanasundar.g@siddhanintelligence.com>
2023-08-24 21:14:20 +0530

    'checking'

--- django_angular_proto/urls.py
+++ django_angular_proto/urls.py
@@ -22,5 +22,4 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('health', views.health_check, name = "health_check"),
-    path('', TemplateView.as_view(template_name='index.html')),
 ]
