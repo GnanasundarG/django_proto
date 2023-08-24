78f0b8978735543b1ed81ceac3e3b33be00c71c9
sundar_g <gnanasundar.g@siddhanintelligence.com>
2023-08-24 23:56:13 +0530

    'changing_template'

--- django_angular_proto/settings.py
+++ django_angular_proto/settings.py
@@ -59,6 +59,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'APP_DIRS': True,
+        'DIRS': [
+            os.path.join(BASE_DIR,'static')
+        ],
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -122,6 +125,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Path to your Angular build output
 ]
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
